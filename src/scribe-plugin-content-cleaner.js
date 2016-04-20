66de372317d3f950101207fdd3a33013140da039
Robert Rees <robert.rees@guardian.co.uk>
2016-04-20 13:36:09 +0100

    Soft hyphens: remove soft hyphens from text
    The &shy; entity is handled inconsistently between Chrome and Firefox and is causing havoc. We don't want to support these so this changes the cleaner to strip them out.
    
    The cleaning expression comes from this Stack Overflow answer:
    
    http://stackoverflow.com/questions/34043050/remove-shy-soft-hyphen-entity-from-element/34043103#34043103

--- src/scribe-plugin-content-cleaner.js
+++ src/scribe-plugin-content-cleaner.js
@@ -5,6 +5,7 @@ module.exports = function(scribe) {
 
     var filters = [
         (text) => text.replace(/(&nbsp;)/gi, " "),
+        (text) => text.replace(/(\&shy;|­|&#173;)/gi, ""),
         (text) => text.replace(/\s+/g, " "),
         (text) => text.replace(/ -- /g, " &ndash; "),
         (text) => text.replace(/ - /g, " &ndash; "),
@@ -17,6 +18,7 @@ module.exports = function(scribe) {
 
 
     return function (scribe) {
+        console.log('Initialising plugin');
         // for now this just exposes a list of
         // commands to do the text cleanup, rather than doing it some
         // weird way with sanitizers
@@ -24,6 +26,8 @@ module.exports = function(scribe) {
 
         cleanupCommand.execute = () => {
             var content = scribe.el.innerHTML;
+
+            console.log(content);
 
             var temp = filters.reduce((val, fn) => {
                 return fn(val);
